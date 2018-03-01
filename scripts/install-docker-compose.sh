9afd58bbcf4f690a1c106158358e1d4b8ab0408b
Javier López <m@javier.io>
2018-03-01 15:20:43 -0600

    scripts/install-docker-compose.sh: install latest version

--- scripts/install-docker-compose.sh
+++ scripts/install-docker-compose.sh
@@ -3,7 +3,9 @@ set -xe
 
 if ! command -v "docker-compose" >/dev/null 2>&1; then
     wget -O /usr/bin/docker-compose \
-        https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m`
+        "$(wget -q -O- https://api.github.com/repos/docker/compose/releases/latest | \
+            awk '/browser_download_url/ {gsub("\"", ""); print $2}' | \
+            grep Linux | grep -v sha)"
 fi
 
 chmod +x /usr/bin/docker-compose
